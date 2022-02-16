58a32678670f16ef98a10e3f31e7799ac2f7015f
Dockside-1 <97267861+Dockside-1@users.noreply.github.com>
2022-02-16 16:22:16 -0500

    Update USXToken.sol
    Name change

--- USXToken.sol
+++ USXToken.sol
@@ -764,7 +764,7 @@ contract USXToken is ERC20, Ownable {
     	uint256 gas,
     	address indexed processor
     );
-    constructor() ERC20("Test Token", "TSTT") {
+    constructor() ERC20("USX Quantum", "USX") {
     	//IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
         // CHANGE - TestNet
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3);
